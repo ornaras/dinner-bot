97359c974a8b557b54f57e05418371d32e077a90
ornaras <ornaras.us@gmail.com>
2025-07-29 22:57:20 +0400

    Добавлен генератор клиента Telegram-бота

--- DinnerBot/Program.cs
+++ DinnerBot/Program.cs
@@ -1,7 +1,11 @@
 using DinnerBot.Services;
+using Telegram.Bot;
 using Telegram.Bot.Polling;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<ITelegramBotClient>
+    (new TelegramBotClient(builder.Configuration["TELEGRAM_TOKEN"]!));
 builder.Services.AddSingleton<IUpdateHandler, UpdateHandler>();
 builder.Services.AddHostedService<PollingService>();
 
